96a8f996295b105f4e9adf5b1d71254efc09e0e0
IvanAgafon <ivanagafon95@gmail.com>
2025-04-02 19:10:25 +0300

    About, Feedback

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import CarouselFadeExample from '../ui/Carousel';
+import About from '../ui/About';
+import Feedback from '../ui/Feedback';
 
 export default function MainPage() {
-  return <div style={{ backgroundColor: 'black', height: '100vh' }}><CarouselFadeExample/>;</div>
+  return (
+    <div style={{ backgroundColor: 'black', height: '100vh' }}>
+      <CarouselFadeExample />;
+      <About />
+      <Feedback />
+    </div>
+  );
 }

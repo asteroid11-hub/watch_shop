e9b41bf74dc966e38cae2b351469f4ce56dd46a6
IvanAgafon <ivanagafon95@gmail.com>
2025-04-03 11:14:41 +0300

    3D start

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CarouselFadeExample from '../ui/Carousel';
 import About from '../ui/About';
 import Feedback from '../ui/Feedback';
+import WatchScene from '../ui/animation';
 
 export default function MainPage() {
   return (
@@ -9,6 +10,7 @@ export default function MainPage() {
       <CarouselFadeExample />;
       <About />
       <Feedback />
+      <WatchScene />
     </div>
   );
 }

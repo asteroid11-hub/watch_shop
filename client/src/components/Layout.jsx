ac6fd83d7f4ddf9f937b35900a47480d6cb2f787
asteroid11-hub <asteroid11@live.com>
2025-04-04 16:07:20 +0300

    deeploy

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-import NavBar from './ui/NavBar'
+import NavBar from './ui/NavBar';
 
-const Layout = ({isLoggedIn,logoutHandler}) => {
+const Layout = ({ isLoggedIn, logoutHandler }) => {
   return (
     <div className="layout">
       <header>
-       <NavBar isLoggedIn={isLoggedIn} logoutHandler={logoutHandler}/>
+        <NavBar isLoggedIn={isLoggedIn} logoutHandler={logoutHandler} />
       </header>
       <Outlet />
-      <footer>
-        <p>Footer</p>
-      </footer>
     </div>
   );
 };

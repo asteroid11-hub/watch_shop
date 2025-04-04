a19cff2c9c239850e64cc946ddbc12973abe2fbf
Nuri-engineer <abdurahmon_1989@mail.ru>
2025-04-04 18:12:36 +0300

    edit

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import NavBar from './ui/NavBar';
 
-const Layout = ({ isLoggedIn, logoutHandler }) => {
+const Layout = ({ isLoggedIn, logoutHandler, user }) => {
   return (
     <div className="layout">
       <header>
-        <NavBar isLoggedIn={isLoggedIn} logoutHandler={logoutHandler} />
+        <NavBar isLoggedIn={isLoggedIn} logoutHandler={logoutHandler} user={user} />
       </header>
       <Outlet />
       <footer></footer>
